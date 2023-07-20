cf31f36b236a8fdbabfde3a717aa5120ba91e7d2
SaiNavyaPallerla <130306501+SaiNavyaPallerla@users.noreply.github.com>
2023-07-20 16:17:50 +0530

    Add files via upload

---
+++ SortedArray.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class SortedArray
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        int numbers[]=new int[size];
+        for(int i=0;i<size;i++)
+        {
+            numbers[i]=sc.nextInt();
+        }
+        boolean isAscending=true;
+        for(int i=0;i<numbers.length-1;i++)
+        { if(numbers[i]>numbers[i+1])
+        {
+            isAscending=false;
+        }
+        }
+    
+    if(isAscending)
+    {
+        System.out.println("Ascending order");
+    }
+    else{
+        System.out.println("Descending order");
+    }
+    sc.close();
+    }
+}
